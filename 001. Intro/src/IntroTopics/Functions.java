dbc24556065f832e644e8fabdf189e23ce6d9424
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-07-05 01:36:05 +0600

    add: functions in java

---
+++ 001. Intro/src/IntroTopics/Functions.java
@@ -0,0 +1,14 @@
+package IntroTopics;
+
+public class Functions {
+    public static void main(String[] args){
+        fun(5);
+    }
+
+    public static void fun(int i){
+        for(int j=0; j<i; j++){
+            System.out.println("Hello " + j);
+        }
+    }
+
+}
